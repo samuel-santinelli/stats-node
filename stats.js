ef674d3fc3e9460cfd8dfd3dd42b578b2351579d
samuel-santinelli <samuelsantinelli@gmail.com>
2022-01-24 10:24:14 -0300

    Enviando alterações| arquivo para testar o node

--- stats.js
+++ stats.js
@@ -1,5 +1,19 @@
 const os = require('os')
 
-const { freemen, totalmem } = os
+setInterval(() => {
+    const { freemem, totalmem } = os
 
-console.log(`${parseInt(freemen() / 1024 / 1024)} MB` , totalmem())
+    const result = parseInt(totalmem() / 1024 / 1024)
+    const memory = parseInt(freemem() / 1024 / 1024)
+    const percents = parseInt((memory / result) * 100)
+    
+    const stats = {
+        free: `${memory} MB`,
+        result: `${result} MB`,
+        usage: `${percents}%`
+    }
+    console.clear()
+    console.log("=== PC STATS ===")
+    console.log(stats)
+}, 1000)
+
